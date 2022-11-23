e8c578cae26c9c039f16ce576f32f24281357bdb
facindito <facinditto@gmail.com>
2022-11-23 19:50:22 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='min-h-screen grid place-content-center'>
+      <h1 className='text-5xl font-bold'>Any Meal</h1>
+    </div>
+  )
+}
+
+export default App
